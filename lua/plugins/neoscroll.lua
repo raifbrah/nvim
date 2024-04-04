d57143f5bed7c89d2e9aeda5f12ab845a2154efd
raifbrah <raifbrah@gmail.com>
2024-04-04 20:48:17 +0300

    Update

--- lua/plugins/neoscroll.lua
+++ lua/plugins/neoscroll.lua
@@ -5,8 +5,6 @@ return {
 
     local t = {}
     -- Syntax: t[keys] = {function, {function arguments}}
-    -- t['<C-u>'] = {'scroll', {'-vim.wo.scroll', 'true', '250'}}
-    -- t['<C-d>'] = {'scroll', { 'vim.wo.scroll', 'true', '250'}}
     t['<C-u>'] = {'scroll', {'-vim.wo.scroll', 'true', '100'}}
     t['<C-d>'] = {'scroll', { 'vim.wo.scroll', 'true', '100'}}
     t['<C-b>'] = {'scroll', {'-vim.api.nvim_win_get_height(0)', 'true', '450'}}
